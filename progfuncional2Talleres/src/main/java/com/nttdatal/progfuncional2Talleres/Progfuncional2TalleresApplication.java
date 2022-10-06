e589c9afc8ac516cbf3f3c0bb98a71ffe2317de0
Guillermo <claseswilly@gmail.com>
2022-10-06 10:43:02 +0200

    Commit 2

--- progfuncional2Talleres/src/main/java/com/nttdatal/progfuncional2Talleres/Progfuncional2TalleresApplication.java
+++ progfuncional2Talleres/src/main/java/com/nttdatal/progfuncional2Talleres/Progfuncional2TalleresApplication.java
@@ -23,7 +23,7 @@ public class Progfuncional2TalleresApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		List<Product> shoppingCart = List.of(
+		List<Product> carro = List.of(
 				new Product("Clothes", 5.90, Tax.NORMAL),
 				new Product("Bread",15.90, Tax.SUPERREDUCED),
 				new Product("Meat",13.99, Tax.REDUCED),
@@ -31,9 +31,14 @@ public class Progfuncional2TalleresApplication implements CommandLineRunner{
 				new Product("Coke",1.89, Tax.REDUCED),
 				new Product("Whiskey",19.90, Tax.NORMAL));
 				
-				Optional<Double> cantidad = shoppingCart.stream()
+				Optional<Double> cantidad = carro.stream()
 				.map(product -> product.price).reduce((x,y)-> x+y);
 		System.out.println("Cantidad total: "+cantidad.get()+ "€");
+		
+		Optional<Double> cantidadTax = carro.stream()
+				.map(product -> product.price + ((product.price *product.tax.percent)/100))
+				.reduce((x, y)-> x+y);
+		System.out.println("La cantidad total con impuestos incluidos es: "+ cantidadTax.get() + "€");
 
 	}
 		
